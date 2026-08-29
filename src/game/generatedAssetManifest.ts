e778b313182f44cf6ccec10724bb82934ff7facc
Grzegorz <grzegorz2047@gmail.com>
2026-08-29 10:56:37 +0200

    art: complete generated Roosevelt asset manifest

--- src/game/generatedAssetManifest.ts
+++ src/game/generatedAssetManifest.ts
@@ -1,8 +1,11 @@
+import cctvDesk from './generatedTextures/cctvDesk';
 import detective from './generatedTextures/characterDetective';
 import irena from './generatedTextures/characterIrena';
 import { character_kamil as kamil, character_marek as marek, character_nina as nina } from './generatedTextures/charactersRemaining';
+import cleaningCart from './generatedTextures/cleaningCart';
 import { floor_guest as floorGuest, floor_lobby as floorLobby, floor_service as floorService } from './generatedTextures/floors';
 import { laundry } from './generatedTextures/laundry';
+import luggageCart from './generatedTextures/luggageCart';
 import { reception } from './generatedTextures/reception';
 import { elevator, stairs } from './generatedTextures/verticalModules';
 import { wall_door307 as room307, wall_ne as wallNe, wall_nw as wallNw } from './generatedTextures/walls';
@@ -22,6 +25,9 @@ export const GENERATED_ROOSEVELT_ASSETS = {
   reception,
   stairs,
   laundry,
+  cctvDesk,
+  cleaningCart,
+  luggageCart,
   wallNw,
   wallNe,
   floorLobby,
